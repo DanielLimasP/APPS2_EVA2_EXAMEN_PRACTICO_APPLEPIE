ef09daa70c51cab60bcc94df8bd8c27594312f56
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2020-04-27 17:07:17 -0600

    Added a user and files activities. Finished user layout
    //TODO
    Add functionality to the user activity

---
+++ app/src/main/java/com/example/apps2_eva2_examen_practico_applepie/FilesActivity.java
@@ -0,0 +1,14 @@
+package com.example.apps2_eva2_examen_practico_applepie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FilesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_files_activity);
+    }
+}
--- app/src/main/java/com/example/apps2_eva2_examen_practico_applepie/MainActivity.java
+++ app/src/main/java/com/example/apps2_eva2_examen_practico_applepie/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.apps2_eva2_examen_practico_applepie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent usersIntent, filesIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void launchUsersActivity(View view){
+        usersIntent = new Intent(this, UsersActivity.class);
+        startActivity(usersIntent);
+    }
+
+    public void launchFilesActivity(View view){
+        filesIntent = new Intent(this, FilesActivity.class);
+        startActivity(filesIntent);
+    }
+
+    public void closeApp(View view){
+        System.exit(1);
+    }
+
 }
