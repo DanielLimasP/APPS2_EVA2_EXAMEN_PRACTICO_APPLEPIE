c7b32df36d7233853d26d2d6c4a7b68abb4bc7ab
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2020-04-27 16:21:47 -0600

    Initial commit
    //TODO:
    
    part 1 part 2
    
    Give access to Kevin and Sergio...

---
+++ app/src/main/java/com/example/apps2_eva2_examen_practico_applepie/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.apps2_eva2_examen_practico_applepie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
