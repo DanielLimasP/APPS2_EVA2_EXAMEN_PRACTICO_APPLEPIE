3c05785afe2bc6ceaa2320120e647eb2c8c6c7ca
miwelomali <pollopower74@gmail.com>
2020-04-28 17:42:40 -0600

    Agregado el almacenamiento fisico

---
+++ app/src/main/java/com/example/apps2_eva2_examen_practico_applepie/FileList.java
@@ -0,0 +1,13 @@
+package com.example.apps2_eva2_examen_practico_applepie;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FileList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_list);
+    }
+}
